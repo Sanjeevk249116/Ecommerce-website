7a9be5096eb515a8af5f1c604084ab1f6758e7c0
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2023-11-25 21:11:29 +0530

    add

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 function Login() {
   return (
-    <>
+    <div>
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         
@@ -70,7 +70,7 @@ function Login() {
           </p>
         </div>
       </div>
-    </>
+    </div>
       
     
   )
